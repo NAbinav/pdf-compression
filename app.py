0e2ca71685ce518f5af433f3e73c5cd9a963dcee
NAbinav <148851259+NAbinav@users.noreply.github.com>
2024-07-03 09:50:39 +0530

    Update app.py
    for temporary storage

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['UPLOAD_FOLDER'] = '/tmp/uploads/'
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() == 'pdf'
